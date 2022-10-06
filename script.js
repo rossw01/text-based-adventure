9237343a7fa2772d143837751b10b2e9ac6b0f38
rossw01 <56947241+rossw01@users.noreply.github.com>
2022-10-06 12:03:38 +0100

    added item pickup and room item requirements

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const textElement = document.getElementById("text");
 const alertElement = document.getElementById("alert");
 
 var currentRoom;
+var inventory = {};
+
+function addToInventory(item) {
+  inventory[item._name] = item;
+  console.log(inventory);
+}
 
 class Item {
   constructor(name, description) {
@@ -12,11 +18,12 @@ class Item {
 }
 
 class Room {
-  constructor(name, description) {
+  constructor(name, description, requiredItem) {
     this._name = name;
     this._desc = description;
     this._linkedRooms = {};
     this._items = {};
+    this._requiredItem = requiredItem;
   }
 
   linkRoom(direction, room) {
@@ -34,8 +41,9 @@ class Room {
     console.log(this._items);
     if (item in this._items) {
       displayAlert(`Picked up ${item}!`);
-      // TODO: Remove item from room
       // TODO: Add item to inventory (not yet implemented)
+      addToInventory(item);
+      // TODO: Remove item from room
     } else {
       displayAlert(`Can't find a ${item}!`);
     }
@@ -44,19 +52,37 @@ class Room {
   move(direction) {
     console.log(this._linkedRooms);
     if (direction in this._linkedRooms) {
-      currentRoom = this._linkedRooms[direction];
-      console.log(`You are now in the ${currentRoom._name}`);
-      return this._linkedRooms[direction];
+      // If room to move to exists
+      if (
+        // If item is required to move in certain direction and you dont have the item....
+        typeof this._linkedRooms[direction]._requiredItem !== "undefined" &&
+        !(this._requiredItem in inventory)
+      ) {
+        displayAlert(
+          `You are going to need a ${this._linkedRooms[direction]._requiredItem._name} to go in here!`
+        );
+        return currentRoom; // Didn't move so we return currentRoom
+      } else {
+        currentRoom = this._linkedRooms[direction];
+        console.log(`You are now in the ${currentRoom._name}`);
+        return this._linkedRooms[direction];
+      }
     }
     displayAlert(`Cannot move ${direction}!`);
     return currentRoom;
   }
 }
 
+const Apple = new Item("Apple", "It's red and looks delicous");
+const Key = new Item("Key", "Unlocks a door");
+
 const Lounge = new Room(
   "Lounge",
-  "You are now in the lounge, there is a big sofa."
+  "You are now in the lounge, there is a big sofa.",
+  Key
 );
+
+console.log(Lounge._requiredItem);
 
 const Hallway = new Room(
   "Hallway",
@@ -69,14 +95,16 @@ const Kitchen = new Room(
   "You are in the kitchen, there is a table and a countertop"
 );
 
-const Apple = new Item("Apple", "It's red and looks delicous");
-
 Kitchen.placeItem("Red apple", Apple);
-
+Kitchen.placeItem("Key", Key);
 // TODO: Separate these
+
 Kitchen.linkRoom("south", Hallway);
-Kitchen.linkRoom("east", Kitchen);
+Kitchen.linkRoom("east", Lounge);
+
 Hallway.linkRoom("north", Kitchen);
+
+Lounge.linkRoom("west", Kitchen);
 
 function displayAlert(text) {
   alertElement.innerText = text;
@@ -104,11 +132,12 @@ document.addEventListener("keydown", function (event) {
       inputElement.value = "";
     } else if (command.split(" ")[0].toLowerCase() == "get") {
       // GET
-      inputElement.innerText = "";
+      inputElement.value = "";
       let item = command.substring(command.indexOf(" ") + 1).toLowerCase();
       currentRoom.get(item.charAt(0).toUpperCase() + item.substring(1)); // Capitalises first letter of item to get
     } else {
       alert("That is not a valid command please try again");
+      inputElement.value = "";
     }
   }
 });
